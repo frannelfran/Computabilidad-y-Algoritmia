e2f62eb25ab5e14d7c468b7e149767d463ed3162
Franco Alla <alu0101571669@ull.edu.es>
2023-09-18 10:46:36 +0100

    18-09-2023

--- califications.h
+++ califications.h
@@ -9,8 +9,9 @@ class Califications {
 
     public:
     Califications(ifstream&); // Constructor de la clase
-    std::map<string, double> add(string, double);
+    std::map<string, double> add(string, double); // Agregar un alumno
     void write(); // Imprimir por pantalla la lista
+
     private:
     std::map<std::string, double> lista_;
 };
