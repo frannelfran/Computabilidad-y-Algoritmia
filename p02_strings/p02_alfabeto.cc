faf7ddd46fbb781935d469952b7921fd08aec515
usuario <alu0101571669@ull.edu.es>
2023-09-26 11:38:57 +0000

    26-09-2023

---
+++ p02_strings/p02_alfabeto.cc
@@ -0,0 +1,17 @@
+#include "p02_alfabeto.h"
+
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Práctica 2: Operaciones con cadenas
+// Autor: Franco Alla
+// Correo: alu0101571669@ull.edu.es
+// Fecha: 25/09/2023
+// Archivo p02_alfabeto.cc
+// Descripción: Implementación de la clase alfabeto
+
+Alfabeto::Alfabeto(set<char>& mi_alfabeto) {
+  alfabeto_ = mi_alfabeto;
+}
---
+++ p02_strings/p02_alfabeto.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Práctica 2: Operaciones con cadenas
+// Autor: Franco Alla
+// Correo: alu0101571669@ull.edu.es
+// Fecha: 26/09/2023
+// Archivo p02_alfabeto.h
+// Descripción: Declaración de la clase cadena
+
+class Alfabeto {
+  public:
+  Alfabeto(set<char>&); // Constructor de la clase
+  private:
+  set<char> alfabeto_; // Atributo privado de la clase
+};
