275a5f37558ee1634c82084591d072c4d6bad042
usuario <alu0101571669@ull.edu.es>
2023-09-28 14:42:22 +0000

    28-09-2023

--- p02_strings/p02_strings.cc
+++ p02_strings/p02_strings.cc
@@ -20,14 +20,23 @@ Cadena::Cadena(const string mi_cadena) { // Constructor de la clase
 
 set<char> Cadena::alfabeto() { //Imprime el alfabeto asociado a la cadena
   set<char> mi_alfabeto;
-  for(int it = 0; it < cadena_.size(); it++) { // Recorre la cadena para crear el alfabeto
-    mi_alfabeto.insert(cadena_[it]);
+  if(cadena_[0] == '&') {
+    return mi_alfabeto;
+  }
+  else {
+    for(int it = 0; it < cadena_.size(); it++) { // Recorre la cadena para crear el alfabeto
+      mi_alfabeto.insert(cadena_[it]);
+    }
   }
   return mi_alfabeto;
 }
 
 int Cadena::longitud() { // Devuelve la longitud de cada cadena
-  return cadena_.size();
+  int longitud = 0;
+  if(cadena_[0] == '&') {
+    return longitud;
+  }
+  else return cadena_.size();
 }
 
 void Cadena::inversa() { // Muestra la cadena inversa
@@ -40,22 +49,34 @@ void Cadena::inversa() { // Muestra la cadena inversa
 
 set<string> Cadena::prefijos() { // Crea el conjunto de prefijos de la cadena
   set<string> conjuntoPrefijos; // Creamos el set donde se van a almacenar los prefijos de la cadena
-  string prefijo;
-  conjuntoPrefijos.insert("&"); // Inserto la cadena vacía
-  for(char letra : cadena_) {
-    prefijo += letra; // A cada prefijo le vamos agregando una letra
-    conjuntoPrefijos.insert(prefijo); // Vamos insertando los prefijos en el set de strings
+  if(cadena_[0] == '&') {
+    conjuntoPrefijos.insert("&");
+    return conjuntoPrefijos;
+  }
+  else {
+    string prefijo;
+    conjuntoPrefijos.insert("&"); // Inserto la cadena vacía
+    for(char letra : cadena_) {
+      prefijo += letra; // A cada prefijo le vamos agregando una letra
+      conjuntoPrefijos.insert(prefijo); // Vamos insertando los prefijos en el set de strings
+    }
   }
   return conjuntoPrefijos;
 }
 
 set<string> Cadena::sufijos() { // Crea el conjunto de sufijos de la cadena
   set<string> conjuntoSufijos;
-  string sufijo;
-  conjuntoSufijos.insert("&"); // Inserto la cadena vacía
-  for (int it = cadena_.size() - 1; it >= 0; it--) {
-    sufijo = cadena_[it] + sufijo;
-    conjuntoSufijos.insert(sufijo); // Vamos insertando los sufijos en el set de strings
+  if(cadena_[0] == '&') {
+    conjuntoSufijos.insert("&");
+    return conjuntoSufijos;
+  }
+  else {
+    string sufijo;
+    conjuntoSufijos.insert("&"); // Inserto la cadena vacía
+    for (int it = cadena_.size() - 1; it >= 0; it--) {
+      sufijo = cadena_[it] + sufijo;
+      conjuntoSufijos.insert(sufijo); // Vamos insertando los sufijos en el set de strings
+    }
   }
   return conjuntoSufijos;
 }
