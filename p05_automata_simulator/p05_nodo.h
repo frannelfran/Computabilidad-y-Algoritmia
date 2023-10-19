bc41f8fe37176f0c026ddb89a0ec6eeca0b83068
usuario <alu0101571669@ull.edu.es>
2023-10-19 07:54:08 +0000

    19/10/2023

--- p05_automata_simulator/p05_nodo.h
+++ p05_automata_simulator/p05_nodo.h
@@ -8,9 +8,8 @@ class Nodo {
   public:
   Nodo(char, int, int); // Constructor de la clase nodo
   Nodo(); // Constructor por defecto
-  bool aceptacion();
   friend std::ostream& operator<<(std::ostream& os, const Nodo& nodo); // Sobrecarga del operador de salida
   private: // Atributos privados de la clase
-  multimap<char, int> transiciones_; 
+  multimap<char, int> transiciones_;
   int aceptacion_;
 };
