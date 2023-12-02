9fdf091e1559b2db4dd5ed1161f448d55e57582a
usuario <alu0101571669@ull.edu.es>
2023-12-02 19:40:28 +0000

    02/12/2023

--- p11_algoritmos_voraces/point_types.h
+++ p11_algoritmos_voraces/point_types.h
@@ -9,5 +9,16 @@
 using namespace std;
 
 namespace CyA {
-  
+  typedef pair<double, double> point; // Representar un punto en el plano
+
+  typedef pair<point, point> line; // Segmento de línea formado 
+  typedef vector<point> point_vector;
+
+  typedef pair<point, point> arc;
+  typedef pair<double, arc> weigthed_arc;
+  typedef vector<weigthed_arc> arc_vector;
+
+  typedef set<point> point_collection;
+
+  typedef vector<arc> tree;
 }
