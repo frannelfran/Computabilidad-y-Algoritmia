d6584718ae60ba644b3aada93d7ab28006a55748
usuario <frannelfran@gmail.com>
2025-09-30 17:07:07 +0000

    feat: Inicialización de los bucles

---
+++ p04_code_analyzer/include/bucle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+/**
+ * @struct Enum para representar los bucles del programa.
+*/
+struct Bucle {
+  string tipo; // for o while
+  int linea; // Linea en la que se declara
+};
