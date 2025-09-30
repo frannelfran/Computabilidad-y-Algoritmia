79c36f6877d0638931f43f8cb89fe11a7aaf70d7
usuario <frannelfran@gmail.com>
2025-09-30 21:48:11 +0000

    feat: Cambiar de struct a clase

--- p04_code_analyzer/include/variable.h
+++ p04_code_analyzer/include/variable.h
@@ -1,14 +1,39 @@
 #pragma once
 #include <string>
+#include <stdexcept>
 
 using namespace std;
 
 /**
- * @struct Enum para representar las variables del programa.
+ * @class Clase para representar las variables del programa.
 */
-struct Variable {
-  string tipo; // double o int
-  int linea; // Linea en la que se declara
-  string nombre; // Nombre de la variable
-  int valor; // Valor de la variable
+class Variable {
+  public:
+    // Constructor y destructor
+    Variable(const string& tipo, const int& linea, const string& nombre, bool inicializada = false) : tipo_(tipo), linea_(linea), nombre_(nombre), inicializada_(inicializada) {}
+    ~Variable() = default;
+
+    inline virtual double getValor() const { throw runtime_error("La variable no está inicializada"); }
+
+  protected:
+    string tipo_;   // Tipo de la variable (int o double)
+    int linea_;     // Línea en la que se declara la variable
+    string nombre_; // Nombre de la variable
+    bool inicializada_; // Indica si la variable ha sido inicializada
+};
+
+/**
+ * @class Clase que representa las variables inicializadas
+ */
+class VariableInicializada : public Variable {
+  public:
+    // Constructor y destructor
+    VariableInicializada() = default;
+    VariableInicializada(const string& tipo, const int& linea, const string& nombre, const double& valor) : Variable(tipo, linea, nombre, true), valor_(valor) {}
+    ~VariableInicializada() = default;
+
+    inline double getValor() const override { return valor_; }
+
+  private:
+    double valor_; // Valor de la variable
 };
