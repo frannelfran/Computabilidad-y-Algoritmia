3692b77839c11d75dcdce487e1516e76b5013356
usuario <frannelfran@gmail.com>
2025-10-01 18:15:36 +0000

    feat: Creación de un struct para almacenar las expresiones regulares

---
+++ p04_code_analyzer/include/expresionRegular.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <regex>
+#include <string>
+
+using namespace std;
+
+/**
+ * @struct Struct que contiene las expresiones regulares utilizadas en el analizador.
+ */
+struct ExpresionRegular {
+  // Expresión para las variables
+  regex variableSinInicializar = regex(R"(^\s*(int|double)\s+([a-zA-Z_]\w*)\s*;)");
+  regex variableInicializada = regex(R"(^\s*(int|double)\s+([a-zA-Z_]\w*)\s*=\s*([^;]+)\s*;)");
+  // Expresion para los bucles
+  regex bucle = regex(R"(^\s*(for|while)\s*\([^)]*\)\s*\{?)");
+  // Expresion para el main
+  regex main = regex(R"(^\s*int\s+main\s*\([^)]*\)\s*\{?)");
+
+  // Comentarios
+  regex comentarioSimple = regex(R"(^\s*//.*)");
+  regex comentarioMultilinea = regex(R"(^\s*/\*.*\*/\s*$)");
+};
